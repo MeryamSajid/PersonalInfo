a93b4756ad640e471b117f77277cc7609f2e9717
MeryamSajid <143551946+MeryamSajid@users.noreply.github.com>
2023-08-30 19:08:03 +0000

    Update

--- PersonalInfo.cs
+++ PersonalInfo.cs
@@ -2,22 +2,32 @@ using System;
 
 class Program {
   public static void Main (string[] args) { 
-    
+
+    // Declaring the variables
+    string name;
+    string birthdate;
+    string workNum;
+    string cellNum;
+
+    // Asking for user input
     Console.Write ("Enter name: ");
     string name = Console.ReadLine();
-    Console.WriteLine(name);
 
-    Console.Write ("Enter birthdate: ");
+    Console.Write ("Enter birthdate (mm/dd/yyyy): ");
     string dateInput = Console.ReadLine();
-    Console.WriteLine(dateInput);
 
     Console.Write ("Enter work phone number: ");
     string workNum = Console.ReadLine();
-    Console.WriteLine("work " + workNum);
-
+    
     Console.Write ("Enter cell phone number: ");
-    string cellNum = Console.ReadLine();
+    string cellNum = Console.ReadLine(); 
+
+    //Info input display on separate lines
+    Console.WriteLine (name);
+    Console.WriteLine(birthdate);
     Console.WriteLine("cell " + cellNum);
+    Console.WriteLine("work " + workNum);
+    
 }
 
 }
